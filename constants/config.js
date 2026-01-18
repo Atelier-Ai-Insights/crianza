4576241cc581a055877a30d2cba041f4f9ec0518
Atelier-Ai-Insights <insights@atelier-consulting.com>
2026-01-18 01:21:37 -0500

    Create config.js

---
+++ constants/config.js
@@ -0,0 +1,6 @@
+// constants/config.js
+export const CONFIG = {
+  APP_NAME: "Mi Estilo Parental",
+  FOOTER_TEXT: "Atelier Consultoría y Estrategia S.A.S - Todos los Derechos Reservados 2025",
+  PRIMARY_COLOR: "#4A69BD" // El azul de tu banner
+};
